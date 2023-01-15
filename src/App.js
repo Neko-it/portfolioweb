fd0580734fa5cba141a39b3e3b6cd4d4ff19a0c1
Neko-it <115939884+Neko-it@users.noreply.github.com>
2023-01-15 16:35:51 +0100

    Creazione

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from "./asset/video.mp4";
 
 function App() {
   return (
     <div className="App">
+  
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <video autoPlay loop muted id='video'>
+            <source src={Video} type='video/mp4' />
+        </video>
       </header>
     </div>
   );
